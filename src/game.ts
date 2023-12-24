4379ef223854abc005f24e2c5139a91f0db41aa6
Reid Jones <reidenglandj@gmail.com>
2023-12-23 22:48:20 -0600

    Snap movement to squares

--- src/game.ts
+++ src/game.ts
@@ -1,4 +1,6 @@
 import * as Phaser from 'phaser';
+type Sprite = Phaser.GameObjects.Sprite;
+type CursorKeys = Phaser.Types.Input.Keyboard.CursorKeys;
 
 export default class Demo extends Phaser.Scene
 {
@@ -7,7 +9,10 @@ export default class Demo extends Phaser.Scene
         super('demo');
     }
 
-    player: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
+    player: Sprite;
+    isPlayerMoving: boolean;
+    currentMovementDirection: integer;
+    pixelsLeftInCurrentMovement: integer;
 
     preload ()
     {
@@ -24,12 +29,14 @@ export default class Demo extends Phaser.Scene
         this.add.image(0, 0, 'background').setOrigin(0);
 
         // Initialize player in the middle of a square (75 x 75)
-        this.player = this.physics.add.sprite(21, 13, 'dude').setOrigin(0);
-
+        this.player = this.add.sprite(21, 13, 'dude').setOrigin(0);//this.physics.add.sprite(21, 13, 'dude').setOrigin(0);
         this.createPlayerAnimations(this);
+        this.isPlayerMoving = false;
+        this.currentMovementDirection = null;
+        this.pixelsLeftInCurrentMovement = 0;
     }
 
-    createPlayerAnimations(scene)
+    createPlayerAnimations(scene: Phaser.Scene)
     {
         scene.anims.create({
             key: 'left',
@@ -54,31 +61,121 @@ export default class Demo extends Phaser.Scene
 
     update ()
     {
-        var cursors = this.input.keyboard.createCursorKeys();
+        this.updatePlayer();
+        // if (cursors.left.isDown)
+        // {
+        //     this.player.anims.play('left', true);
+        // }
+        // else if (cursors.right.isDown)
+        // {
+        //     this.player.anims.play('right', true);
+        // }
+        // else
+        // {
+        //     this.player.anims.play('turn');
+        // }
 
+        // if (cursors.up.isDown && this.player.body.touching.down)
+        // {
+
+        // }
+    }
+
+    /*
+        Steps for player movement:
+        1) If player is not currently moving, check if movement key is down
+        2) Start player movement animation, move player sprite to the next square
+        3) When the player sprite moves one square, stop the animation at the resting position 
+    */
+    updatePlayer()
+    {
+        var cursors = this.input.keyboard.createCursorKeys();
+        var playerMovementKeyDown = null;
         if (cursors.left.isDown)
         {
-            this.player.setVelocityX(-160);
-
-            this.player.anims.play('left', true);
+            playerMovementKeyDown = MovementDirection.LEFT;
         }
         else if (cursors.right.isDown)
         {
-            this.player.setVelocityX(160);
+            playerMovementKeyDown = MovementDirection.DOWN;
+        }
+        else if (cursors.up.isDown)
+        {
+            playerMovementKeyDown = MovementDirection.UP;
+        }
+        else if (cursors.down.isDown)
+        {
+            playerMovementKeyDown = MovementDirection.DOWN;
+        }
 
+        // Start moving in a direction
+        if (!this.isPlayerMoving && playerMovementKeyDown)
+        {
+            this.isPlayerMoving = true;
+            this.updatePlayerDirection(cursors);
+        }
+
+        //If moving, continue moving in the direction
+        if (this.isPlayerMoving)
+        {
+            this.movePlayer();
+        }
+    }
+
+    updatePlayerDirection(keysDown: CursorKeys)
+    {
+        this.isPlayerMoving = true;
+        this.pixelsLeftInCurrentMovement = PIXELS_TO_MOVE;
+
+        if (keysDown.left.isDown)
+        {
+            this.currentMovementDirection = MovementDirection.LEFT;
+            this.player.anims.play('left', true);
+        }
+        else if (keysDown.right.isDown)
+        {
+            this.currentMovementDirection = MovementDirection.RIGHT;
             this.player.anims.play('right', true);
         }
-        else
+        else if (keysDown.up.isDown)
         {
-            this.player.setVelocityX(0);
+            this.currentMovementDirection = MovementDirection.UP;
+            this.player.anims.play('left', true);
+        }
+        else if (keysDown.down.isDown)
+        {
+            this.currentMovementDirection = MovementDirection.DOWN;
+            this.player.anims.play('right', true);
+        }
+    }
 
+    movePlayer()
+    {
+        if (this.pixelsLeftInCurrentMovement <= 0)
+        {
+            this.isPlayerMoving = false;
             this.player.anims.play('turn');
+            return;
         }
 
-        if (cursors.up.isDown && this.player.body.touching.down)
+        if (this.currentMovementDirection == MovementDirection.LEFT)
         {
-            this.player.setVelocityY(-500);
+            this.player.x -= MOVEMENT_SPEED;
         }
+        else if (this.currentMovementDirection == MovementDirection.RIGHT)
+        {
+            this.player.x += MOVEMENT_SPEED;
+        }
+        else if (this.currentMovementDirection == MovementDirection.UP)
+        {
+            this.player.y -= MOVEMENT_SPEED;
+        }
+        else if (this.currentMovementDirection == MovementDirection.DOWN)
+        {
+            this.player.y += MOVEMENT_SPEED;
+        }
+
+        this.pixelsLeftInCurrentMovement -= MOVEMENT_SPEED;
     }
 }
 
@@ -92,5 +189,17 @@ const config = {
     },
     scene: Demo
 };
+
+const MOVEMENT_SPEED = 5;
+
+// Size of each square (600 pixels / 8 squares)
+const PIXELS_TO_MOVE = 75;
+
+const MovementDirection = {
+    'UP': 1,
+    'LEFT': 2,
+    'RIGHT': 3,
+    'DOWN': 4
+}
 
 const game = new Phaser.Game(config);
