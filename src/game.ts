86ac0a64702b6d6772f4d0b71f55c72c9aee2ee2
Reid Jones <reidenglandj@gmail.com>
2024-01-02 19:04:12 -0800

    Import beat detection from demo

--- src/game.ts
+++ src/game.ts
@@ -1,6 +1,8 @@
 import * as Phaser from 'phaser';
 type Sprite = Phaser.GameObjects.Sprite;
 type CursorKeys = Phaser.Types.Input.Keyboard.CursorKeys;
+type CollidableGameObject = Phaser.GameObjects.GameObject & {collided?: boolean}
+type Note = Phaser.GameObjects.GameObject & {timestamp?: number, spawned?: boolean}
 
 export default class Demo extends Phaser.Scene
 {
@@ -9,13 +11,29 @@ export default class Demo extends Phaser.Scene
         super('demo');
     }
 
+    // Player
     player: Sprite;
     isPlayerMoving: boolean;
-    currentMovementDirection: integer;
+    isDirectionKeyDown: boolean;
+    currentPlayerDirection: integer;
     pixelsLeftInCurrentMovement: integer;
+
+    // Sound management
+    noteBar: Phaser.GameObjects.Rectangle;
+    noteCollisionBox: Phaser.GameObjects.Rectangle;
+    song: Phaser.Sound.HTML5AudioSound | Phaser.Sound.NoAudioSound | Phaser.Sound.WebAudioSound;
+    colliders: Array<CollidableGameObject>;
+    lastNoteIndex: number;
+    notesTimestamps: Note[];
+    notesSpawned: Phaser.GameObjects.Arc[];
+    musicStartTime: number;
+    timeToMove: number;
+    score: number;
+    scoreText: Phaser.GameObjects.Text;
 
     preload ()
     {
+        this.load.audio("song", "assets/galactic_dancing.ogg");
         this.load.image('background', 'assets/background.png');
         this.load.spritesheet('dude', 
             'assets/dude.png',
@@ -25,15 +43,38 @@ export default class Demo extends Phaser.Scene
 
     create ()
     {
-        // this.add.image(400, 300, 'sky');
         this.add.image(0, 0, 'background').setOrigin(0);
 
         // Initialize player in the middle of a square (75 x 75)
-        this.player = this.add.sprite(21, 13, 'dude').setOrigin(0);//this.physics.add.sprite(21, 13, 'dude').setOrigin(0);
+        this.player = this.add.sprite(21, 13, 'dude').setOrigin(0);
         this.createPlayerAnimations(this);
+        this.player.anims.play('turn');
         this.isPlayerMoving = false;
-        this.currentMovementDirection = null;
+        this.isDirectionKeyDown = false;
+        this.currentPlayerDirection = MovementDirection.NONE;
         this.pixelsLeftInCurrentMovement = 0;
+
+        this.noteBar = this.add.rectangle(WINDOW_WIDTH / 2, WINDOW_HEIGHT - 30, WINDOW_WIDTH, 10, 0xff0000);
+        this.noteCollisionBox = this.add.rectangle(WINDOW_WIDTH / 2, WINDOW_HEIGHT - 30, 20, 60, 0xffff00);
+        this.noteCollisionBox.setStrokeStyle(2, 0x00ff00);
+        this.lastNoteIndex = 0;
+
+        this.notesTimestamps = [];
+        this.notesSpawned = [];
+        this.timeToMove = 1000; // ms, time for the note to go to the bottom. The lower the faster/hardest
+        this.notesTimestamps = JSON.parse('[{"timestamp":944},{"timestamp":1383},{"timestamp":1768},{"timestamp":2173},{"timestamp":2364},{"timestamp":2562},{"timestamp":2951},{"timestamp":3560},{"timestamp":3767},{"timestamp":3969},{"timestamp":4164},{"timestamp":4565},{"timestamp":4760},{"timestamp":4971},{"timestamp":5377},{"timestamp":5567},{"timestamp":5766},{"timestamp":6163},{"timestamp":6776},{"timestamp":6978},{"timestamp":7181},{"timestamp":7388},{"timestamp":7781},{"timestamp":8174},{"timestamp":8576},{"timestamp":8770},{"timestamp":8965},{"timestamp":9358},{"timestamp":9970},{"timestamp":10177},{"timestamp":10376},{"timestamp":10570},{"timestamp":10964},{"timestamp":11162},{"timestamp":11365},{"timestamp":11762},{"timestamp":11961},{"timestamp":12184},{"timestamp":12561},{"timestamp":13165},{"timestamp":13380},{"timestamp":13575},{"timestamp":13778},{"timestamp":14167},{"timestamp":14370},{"timestamp":14585},{"timestamp":14978},{"timestamp":15177},{"timestamp":15400},{"timestamp":15764},{"timestamp":16356},{"timestamp":16563},{"timestamp":16766},{"timestamp":16973},{"timestamp":17391},{"timestamp":17594},{"timestamp":17792},{"timestamp":17987},{"timestamp":18181},{"timestamp":18367},{"timestamp":18570},{"timestamp":18765},{"timestamp":19145},{"timestamp":19535},{"timestamp":19750},{"timestamp":19961},{"timestamp":20172},{"timestamp":20573},{"timestamp":20772},{"timestamp":20975},{"timestamp":21372},{"timestamp":21579},{"timestamp":21790},{"timestamp":22167},{"timestamp":22771},{"timestamp":22982},{"timestamp":23185},{"timestamp":23578},{"timestamp":23785},{"timestamp":23988},{"timestamp":24182},{"timestamp":24393},{"timestamp":24592},{"timestamp":24790},{"timestamp":24981},{"timestamp":25378},{"timestamp":25771},{"timestamp":26190}]');
+
+        // We create the audio object and play it
+        this.song = this.sound.add("song");
+        this.song.volume = 0.1;
+        this.song.play();
+        this.musicStartTime = Date.now();
+
+        this.score = 0;
+        this.scoreText = this.add.text(0, WINDOW_HEIGHT - 100, this.score.toString(), { fontFamily: "arial", fontSize: "42px" });
+        this.scoreText.setStroke("#ff0000", 2);
+
+        this.colliders = [];
     }
 
     createPlayerAnimations(scene: Phaser.Scene)
@@ -62,23 +103,11 @@ export default class Demo extends Phaser.Scene
     update ()
     {
         this.updatePlayer();
-        // if (cursors.left.isDown)
-        // {
-        //     this.player.anims.play('left', true);
-        // }
-        // else if (cursors.right.isDown)
-        // {
-        //     this.player.anims.play('right', true);
-        // }
-        // else
-        // {
-        //     this.player.anims.play('turn');
-        // }
 
-        // if (cursors.up.isDown && this.player.body.touching.down)
-        // {
-
-        // }
+        // Sound management
+        this.handlePlayerInput();
+        this.spawnNotes();
+        this.checkNoteCollisions();
     }
 
     /*
@@ -90,26 +119,26 @@ export default class Demo extends Phaser.Scene
     updatePlayer()
     {
         var cursors = this.input.keyboard.createCursorKeys();
-        var playerMovementKeyDown = null;
+        var keyDown = null;
         if (cursors.left.isDown)
         {
-            playerMovementKeyDown = MovementDirection.LEFT;
+            keyDown = MovementDirection.LEFT;
         }
         else if (cursors.right.isDown)
         {
-            playerMovementKeyDown = MovementDirection.DOWN;
+            keyDown = MovementDirection.DOWN;
         }
         else if (cursors.up.isDown)
         {
-            playerMovementKeyDown = MovementDirection.UP;
+            keyDown = MovementDirection.UP;
         }
         else if (cursors.down.isDown)
         {
-            playerMovementKeyDown = MovementDirection.DOWN;
+            keyDown = MovementDirection.DOWN;
         }
 
-        // Start moving in a direction
-        if (!this.isPlayerMoving && playerMovementKeyDown)
+        // Start moving in a new direction
+        if (!this.isPlayerMoving && keyDown != null && keyDown != this.currentPlayerDirection)
         {
             this.isPlayerMoving = true;
             this.updatePlayerDirection(cursors);
@@ -124,27 +153,26 @@ export default class Demo extends Phaser.Scene
 
     updatePlayerDirection(keysDown: CursorKeys)
     {
-        this.isPlayerMoving = true;
         this.pixelsLeftInCurrentMovement = PIXELS_TO_MOVE;
 
         if (keysDown.left.isDown)
         {
-            this.currentMovementDirection = MovementDirection.LEFT;
+            this.currentPlayerDirection = MovementDirection.LEFT;
             this.player.anims.play('left', true);
         }
         else if (keysDown.right.isDown)
         {
-            this.currentMovementDirection = MovementDirection.RIGHT;
+            this.currentPlayerDirection = MovementDirection.RIGHT;
             this.player.anims.play('right', true);
         }
         else if (keysDown.up.isDown)
         {
-            this.currentMovementDirection = MovementDirection.UP;
+            this.currentPlayerDirection = MovementDirection.UP;
             this.player.anims.play('left', true);
         }
         else if (keysDown.down.isDown)
         {
-            this.currentMovementDirection = MovementDirection.DOWN;
+            this.currentPlayerDirection = MovementDirection.DOWN;
             this.player.anims.play('right', true);
         }
     }
@@ -154,48 +182,147 @@ export default class Demo extends Phaser.Scene
         if (this.pixelsLeftInCurrentMovement <= 0)
         {
             this.isPlayerMoving = false;
+            this.currentPlayerDirection = MovementDirection.NONE;
             this.player.anims.play('turn');
             return;
         }
 
-        if (this.currentMovementDirection == MovementDirection.LEFT)
+        // Do not move farther than the remaining pixels left
+        let adjustedMovementSpeed = Math.min(this.pixelsLeftInCurrentMovement, MOVEMENT_SPEED);
+
+        if (this.currentPlayerDirection == MovementDirection.LEFT)
         {
-            this.player.x -= MOVEMENT_SPEED;
+            this.player.x -= adjustedMovementSpeed;
         }
-        else if (this.currentMovementDirection == MovementDirection.RIGHT)
+        else if (this.currentPlayerDirection == MovementDirection.RIGHT)
         {
-            this.player.x += MOVEMENT_SPEED;
+            this.player.x += adjustedMovementSpeed;
         }
-        else if (this.currentMovementDirection == MovementDirection.UP)
+        else if (this.currentPlayerDirection == MovementDirection.UP)
         {
-            this.player.y -= MOVEMENT_SPEED;
+            this.player.y -= adjustedMovementSpeed;
         }
-        else if (this.currentMovementDirection == MovementDirection.DOWN)
+        else if (this.currentPlayerDirection == MovementDirection.DOWN)
         {
-            this.player.y += MOVEMENT_SPEED;
+            this.player.y += adjustedMovementSpeed;
         }
 
-        this.pixelsLeftInCurrentMovement -= MOVEMENT_SPEED;
+        this.pixelsLeftInCurrentMovement -= adjustedMovementSpeed;
+    }
+
+    handlePlayerInput()
+    {
+        var cursors = this.input.keyboard.createCursorKeys();
+        if (cursors.space.isDown) {
+            // we create a new collider at the position of the red bar
+            let collider = this.add.rectangle(WINDOW_WIDTH / 2, WINDOW_HEIGHT - 30, 20, 60, 0xaaaaff);
+
+            // attach physics
+            let colliderWithPhysicsBody = this.physics.add.existing(collider) as CollidableGameObject;
+
+            // little tween to grow
+            this.tweens.add({
+                targets: colliderWithPhysicsBody,
+                scale: 1.5,
+                duration: 100,
+                alpha: 0,
+                onComplete: () => {
+                    colliderWithPhysicsBody.destroy();
+
+                    // If the collider did not hit a note, its a miss, so lets lower the score
+                    if (colliderWithPhysicsBody.collided != true) {
+                        this.cameras.main.shake(100, 0.01);
+                        // this.score -= 200;
+                        // this.updateScoreText();
+                    }
+                }
+            });
+
+            // add the collider to the list
+            this.colliders.push(colliderWithPhysicsBody);
+        }
+    }
+
+    spawnNotes()
+    {
+        // lets look up to the 10 next notes and spawn if needed
+        for (let i = this.lastNoteIndex; i < this.lastNoteIndex + 10; i++) {
+            let note = this.notesTimestamps[i];
+            if (!note) break;
+
+            // Spawn note if: is not already spawned, and the timing is right. From the start of the song, we need to consider the time it takes for the note to fall so we start it at the timestamp minus the time to fall
+            if (
+                note.spawned != true
+                && note.timestamp <= Date.now() - this.musicStartTime + this.timeToMove
+            ) {
+                this.spawnNote();
+                this.lastNoteIndex = i;
+                note.spawned = true;
+            }
+        }
+    }
+
+    spawnNote()
+    {
+        // This is self explanatory. Spawn the note and let it fall to the bottom.
+        let note = this.add.circle(WINDOW_WIDTH, 570, 20, 0xffff00);
+        this.notesSpawned.push(note);
+        this.physics.add.existing(note);
+        this.physics.moveTo(note, 0, 570, null, this.timeToMove);
+    }
+
+    checkNoteCollisions()
+    {
+        this.physics.overlap(this.colliders, this.notesSpawned, (colliderGameObject, noteGameObject) => {
+            let collider = colliderGameObject as CollidableGameObject;
+            let note = noteGameObject as Phaser.GameObjects.Arc;
+
+            // the collider collided
+            collider.collided = true;
+
+            // remove the collider from list
+            this.colliders.splice(this.colliders.indexOf(collider), 1);
+
+            // destroy the note and remove from list
+            note.destroy();
+            this.notesSpawned.splice(this.notesSpawned.indexOf(note), 1);
+
+            // increase the score and update the text
+            this.score += 100;
+            this.updateScoreText();
+        });
+    }
+
+    updateScoreText()
+    {
+        this.scoreText.text = this.score.toString();
     }
 }
+
+const WINDOW_WIDTH = 600;
+const WINDOW_HEIGHT = 600;
 
 const config = {
     type: Phaser.AUTO,
     backgroundColor: '#125555',
-    width: 600,
-    height: 600,
+    width: WINDOW_WIDTH,
+    height: WINDOW_HEIGHT,
     physics: {
-        default: 'arcade'
+        default: 'arcade',
+        arcade: {
+            debug: false
+        }
     },
     scene: Demo
 };
 
-const MOVEMENT_SPEED = 5;
+const MOVEMENT_SPEED = 10;
 
 // Size of each square (600 pixels / 8 squares)
 const PIXELS_TO_MOVE = 75;
 
 const MovementDirection = {
+    'NONE': 0,
     'UP': 1,
     'LEFT': 2,
     'RIGHT': 3,
