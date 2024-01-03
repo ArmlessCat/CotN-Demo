db1aa0bb313411e4452ed81329281cb688485356
Reid Jones <reidenglandj@gmail.com>
2024-01-02 19:59:14 -0800

    [WIP] Start add game board

--- src/game.ts
+++ src/game.ts
@@ -11,6 +11,9 @@ export default class Demo extends Phaser.Scene
         super('demo');
     }
 
+    // Board
+    board: Phaser.GameObjects.GameObject[][];
+
     // Player
     player: Sprite;
     isPlayerMoving: boolean;
@@ -43,10 +46,19 @@ export default class Demo extends Phaser.Scene
 
     create ()
     {
+        // Initialize board
         this.add.image(0, 0, 'background').setOrigin(0);
+        for (let i = 0; i < NUMBER_OF_SQUARES; i++)
+        {
+            this.board[i] = [];
+            for (let j = 0; j < NUMBER_OF_SQUARES; j++)
+            {
+                this.board[i][j] = undefined;
+            }
+        }
 
-        // Initialize player in the middle of a square (75 x 75)
-        this.player = this.add.sprite(21, 13, 'dude').setOrigin(0);
+        // Initialize player
+        this.player = this.add.sprite(21, 13, 'dude').setOrigin(0); // middle of a square (75 x 75)
         this.createPlayerAnimations(this);
         this.player.anims.play('turn');
         this.isPlayerMoving = false;
@@ -54,6 +66,7 @@ export default class Demo extends Phaser.Scene
         this.currentPlayerDirection = MovementDirection.NONE;
         this.pixelsLeftInCurrentMovement = 0;
 
+        // Initialize sound
         this.noteBar = this.add.rectangle(WINDOW_WIDTH / 2, WINDOW_HEIGHT - 30, WINDOW_WIDTH, 10, 0xff0000);
         this.noteCollisionBox = this.add.rectangle(WINDOW_WIDTH / 2, WINDOW_HEIGHT - 30, 20, 60, 0xffff00);
         this.noteCollisionBox.setStrokeStyle(2, 0x00ff00);
@@ -64,7 +77,6 @@ export default class Demo extends Phaser.Scene
         this.timeToMove = 1000; // ms, time for the note to go to the bottom. The lower the faster/hardest
         this.notesTimestamps = JSON.parse('[{"timestamp":944},{"timestamp":1383},{"timestamp":1768},{"timestamp":2173},{"timestamp":2364},{"timestamp":2562},{"timestamp":2951},{"timestamp":3560},{"timestamp":3767},{"timestamp":3969},{"timestamp":4164},{"timestamp":4565},{"timestamp":4760},{"timestamp":4971},{"timestamp":5377},{"timestamp":5567},{"timestamp":5766},{"timestamp":6163},{"timestamp":6776},{"timestamp":6978},{"timestamp":7181},{"timestamp":7388},{"timestamp":7781},{"timestamp":8174},{"timestamp":8576},{"timestamp":8770},{"timestamp":8965},{"timestamp":9358},{"timestamp":9970},{"timestamp":10177},{"timestamp":10376},{"timestamp":10570},{"timestamp":10964},{"timestamp":11162},{"timestamp":11365},{"timestamp":11762},{"timestamp":11961},{"timestamp":12184},{"timestamp":12561},{"timestamp":13165},{"timestamp":13380},{"timestamp":13575},{"timestamp":13778},{"timestamp":14167},{"timestamp":14370},{"timestamp":14585},{"timestamp":14978},{"timestamp":15177},{"timestamp":15400},{"timestamp":15764},{"timestamp":16356},{"timestamp":16563},{"timestamp":16766},{"timestamp":16973},{"timestamp":17391},{"timestamp":17594},{"timestamp":17792},{"timestamp":17987},{"timestamp":18181},{"timestamp":18367},{"timestamp":18570},{"timestamp":18765},{"timestamp":19145},{"timestamp":19535},{"timestamp":19750},{"timestamp":19961},{"timestamp":20172},{"timestamp":20573},{"timestamp":20772},{"timestamp":20975},{"timestamp":21372},{"timestamp":21579},{"timestamp":21790},{"timestamp":22167},{"timestamp":22771},{"timestamp":22982},{"timestamp":23185},{"timestamp":23578},{"timestamp":23785},{"timestamp":23988},{"timestamp":24182},{"timestamp":24393},{"timestamp":24592},{"timestamp":24790},{"timestamp":24981},{"timestamp":25378},{"timestamp":25771},{"timestamp":26190}]');
 
-        // We create the audio object and play it
         this.song = this.sound.add("song");
         this.song.volume = 0.1;
         this.song.play();
@@ -75,6 +87,9 @@ export default class Demo extends Phaser.Scene
         this.scoreText.setStroke("#ff0000", 2);
 
         this.colliders = [];
+
+        // Add initial game objects to the board
+        this.board[0][0] = this.player;
     }
 
     createPlayerAnimations(scene: Phaser.Scene)
@@ -319,6 +334,7 @@ const config = {
 const MOVEMENT_SPEED = 10;
 
 // Size of each square (600 pixels / 8 squares)
+const NUMBER_OF_SQUARES = 8;
 const PIXELS_TO_MOVE = 75;
 
 const MovementDirection = {
